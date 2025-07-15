4c8e12eff6475b03fbf641d8e4e788699dbaf680
qcloud <ubuntu@localhost.localdomain>
2025-07-16 07:02:11 +0800

    init

--- src/views/params/dynamic/index.vue
+++ src/views/params/dynamic/index.vue
@@ -72,7 +72,7 @@ export default defineComponent({
 			router.push({
 				name: 'paramsDynamicDetails',
 				params: {
-					t: 'gfast3.2后台管理系统',
+					t: '榴莲时代',
 					id: state.value,
 					tagsViewName: state.tagsViewName,
 				},
